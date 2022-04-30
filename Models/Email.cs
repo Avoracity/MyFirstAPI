2acb2a7ea77ab1a362ee83f9b68a24147ba99c3b
Michael Alvarez <michael.alvarez687@myhunter.cuny.edu>
2022-04-30 13:28:48 -0400

    Revert "Add project files."
    This reverts commit 7e57964e6e94acca91a92056aab3eca562522bae.

--- Models/Email.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MyFirstAPI.Models
-{
-    public class Email
-    {
-        public int EmailId { get; set; }
-        public string? EmailAddress { get; set; }
-        public bool IsSubscribed { get; set; }
-    }
-}
