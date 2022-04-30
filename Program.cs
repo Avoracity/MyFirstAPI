a76a5fb3a8ba5363fe144a42c8374b25e016e750
Michael Alvarez <michael.alvarez687@myhunter.cuny.edu>
2022-04-30 13:29:58 -0400

    Revert "Revert "Add project files.""
    This reverts commit 2acb2a7ea77ab1a362ee83f9b68a24147ba99c3b.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using MyFirstAPI.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<MyFirstAPIDBContext>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
